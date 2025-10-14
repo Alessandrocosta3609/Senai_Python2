d59b00480a93d9909ff4a341526f13d0f4c9996d
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-10-13 21:53:36 -0300

    exercicios

---
+++ Aula02.py
@@ -0,0 +1,92 @@
+# # Variavel = valor que ela recebe
+
+# nome = "Alessandro\n"
+# sobrenome = "Ferreira"
+
+# fullname = nome + sobrenome
+
+# print (fullname)
+
+# n1 = 10
+# n2 = 4
+# soma = n1+n2
+# print (soma)
+
+# # Declaração de Variável Multipla
+
+# pessoa1 = "Alessandro"
+# pessoa2 = "Kelly"
+# pessoa3 = "Matheus"
+# pessoa4 = "Luiza"
+
+# print(pessoa1)
+# print(pessoa2)
+# print(pessoa3)
+# print(pessoa4)
+
+
+
+# # Alternativa
+
+# pessoa1,pessoa2,pessoa3,pessoa4 = "Alessandro","\nKelly","\nMatheus","\nLuiza"
+
+# print(pessoa1,pessoa2,pessoa3,pessoa4)
+
+# # Atribuindo o mesmo Valor a variaveis diferentes 
+
+
+# fruta1 = "Melancia"
+# fruta2 = "Melancia"
+# fruta3 = "Melancia"
+
+# print(fruta1)
+# print(fruta2)
+# print(fruta3)
+
+# # Alternativa
+
+# fruta1 = fruta2 = fruta3 = "Melancia"
+# print(fruta1)
+# print(fruta2)
+# print(fruta3)
+
+# # Descobrir um Perimetro
+
+# largura = 2
+# altura = 4
+# perimetro = 2 * largura + 2 * altura
+# print(perimetro)
+
+
+# n1 = 25
+# n2 = 35
+
+# print ("Soma: ", n1 + n2)
+# print ("Subtracao: " , n1 - n2)
+# print ("Multiplicacao: " , n1 * n2)
+# print ("Divisao Float: " , n1 / n2)
+# print ("Divisao Inteira: " , n1 // n2)
+# print ("Resto da Divisao: " , n1 % n2)
+# print ("Potenciacao: " , n1 ** n2)
+
+
+# A = 10
+# B = 5
+
+# print(A)
+# print(B)
+
+# A,B = B,A
+
+# print(A)
+# print(B)
+
+
+A = input ("Digite o primeiro valor : ")
+B = input ("Digite o segundo valor : ")
+
+print ( "Valor de A ",A , "Valor de B",B)
+
+
+A,B = B,A
+print ( "Valor de A ",A , "Valor de B",B)
