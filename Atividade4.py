a4c2ef35228050f521aca67e00fdab1e1f0727b2
alessandrocosta3609 <alessandro@dinamicasp.com.br>
2025-10-23 21:52:11 -0300

    atividade 4

--- Atividade4.py
+++ Atividade4.py
@@ -1,3 +1,36 @@
+# Exercicio 3
+
+
+numero = int(input("Digite o número para treinar sua tabuada : "))
+
+acertos = 0
+erros = 0
+
+for i in range(1, 11):
+    resposta = int(input(f"{numero} x {i} = "))
+    resultado_correto = numero * i
+    if resposta == resultado_correto:
+        print("Correto!")
+        acertos += 1
+    else:
+        print(f"Que pena, você errou! O valor correto é {resultado_correto}.")
+        erros += 1
+
+print(f"Total de acertos: {acertos}")
+print(f"Total de erros: {erros}")
+
+
+
+
+
+
+
+
+
+
+# Exercicio 4
+
+
 numero_mais_alto = 0
 numero_mais_baixo = 0
 altura_mais_alta = -1
